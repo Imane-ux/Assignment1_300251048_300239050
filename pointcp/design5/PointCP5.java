d2b53f472250aee0bb1da0da91f359518cb109b6
Imane-ux <iamin053@uottawa.ca>
2022-09-24 14:29:09 -0400

    Adding the 5th design

---
+++ pointcp/design5/PointCP5.java
@@ -0,0 +1,27 @@
+/**
+ * PointCP5 is our abstract class.
+ */
+public abstract class PointCP5 {
+
+  //Instance methods ***
+ 
+ 
+  public abstract double getX();
+  
+  
+  public abstract double getY();
+  
+  
+  public abstract double getRho();
+  
+  
+  public abstract double getTheta();
+ 
+
+  public abstract String toString();
+
+
+
+
+ 
+}
