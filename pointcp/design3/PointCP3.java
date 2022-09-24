78b374438d5faffc47153c556a5831cd2a694f27
Imane-ux <iamin053@uottawa.ca>
2022-09-24 12:05:13 -0400

    just added my 3rd design

---
+++ pointcp/design3/PointCP3.java
@@ -0,0 +1,117 @@
+/**
+ * PointCP3 stores Cartesian only
+ */
+public class PointCP3 {
+
+    //Instance variables ************************************************ 
+  
+  /**
+   * Contains the current value of X 
+   */
+  private double x;
+  
+  /**
+   * Contains the current value of Y value 
+   */
+  private double y;
+	
+  
+  //Constructors ******************************************************
+
+  /**
+   * Constructs a coordinate object, with a type identifier.
+   */
+  public PointCP3(double xOrRho, double yOrTheta)
+  {
+    this.x = xOrRho;
+    this.y = yOrTheta;
+  }
+	
+  
+  //Instance methods **************************************************
+ 
+ 
+  public double getX()
+  {
+    return x;
+  }
+  
+  public double getY()
+  {
+    return y;
+  }
+  
+  public double getRho()
+  {
+    return (Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)));
+  }
+  
+  public double getTheta()
+  {
+    return Math.toDegrees(Math.atan2(y, x));
+  }
+  
+	
+  /**
+   * Converts Cartesian coordinates to Polar coordinates.
+   */
+  public void convertStorageToPolar()
+  {
+
+      //Calculate RHO and THETA
+      double temp = getRho();
+      y = getTheta();
+      x = temp;
+
+
+  }
+	
+
+  /**
+   * Calculates the distance in between two points using the Pythagorean
+   * theorem  (C ^ 2 = A ^ 2 + B ^ 2). Not needed until E2.30.
+   *
+   * @param pointA The first point.
+   * @param pointB The second point.
+   * @return The distance between the two points.
+   */
+  public double getDistance(PointCP3 pointB)
+  {
+    // Obtain differences in X and Y, sign is not important as these values
+    // will be squared later.
+    double deltaX = getX() - pointB.getX();
+    double deltaY = getY() - pointB.getY();
+    
+    return Math.sqrt((Math.pow(deltaX, 2) + Math.pow(deltaY, 2)));
+  }
+
+  /**
+   * Rotates the specified point by the specified number of degrees.
+   * Not required until E2.30
+   *
+   * @param point The point to rotate
+   * @param rotation The number of degrees to rotate the point.
+   * @return The rotated image of the original point.
+   */
+  public PointCP3 rotatePoint(double rotation)
+  {
+    double radRotation = Math.toRadians(rotation);
+    double X = getX();
+    double Y = getY();
+        
+    return new PointCP3(
+      (Math.cos(radRotation) * X) - (Math.sin(radRotation) * Y),
+      (Math.sin(radRotation) * X) + (Math.cos(radRotation) * Y));
+  }
+
+  /**
+   * Returns information about the coordinates.
+   *
+   * @return A String containing information about the coordinates.
+   */
+  public String toString()
+  {
+    return "Stored as Cartesian  ( " + getX() + "," + getY() + ")";
+
+  }
+}
